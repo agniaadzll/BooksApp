19c8f9f128782a479be0322b427e7a45a82efd83
agniaadzll <61041530+agniaadzll@users.noreply.github.com>
2020-11-09 18:07:33 +0700

    BooksApp

---
+++ app/src/main/java/com/example/booksapp/itemData.java
@@ -0,0 +1,10 @@
+package com.example.booksapp;
+
+public class itemData {
+    public String itemTitle;
+    public String itemAuthor;
+    public String itemImage;
+    public String itemDescription;
+
+
+}
